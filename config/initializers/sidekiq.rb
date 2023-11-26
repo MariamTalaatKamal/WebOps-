032ace4c95a2026aa8798311cc9446926cea238e
MariamTalaatKamal <mariam.talaat93@gmail.com>
2023-11-26 12:03:09 +0200

    dockerizing rails app

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,11 @@
 Sidekiq.configure_server do |config|
    config.logger = ActiveSupport::Logger.new(STDOUT)
   end
-  # config/initializers/sidekiq.rb
+# config/initializers/sidekiq.rb
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://redis:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://redis:6379/0' }
 end
